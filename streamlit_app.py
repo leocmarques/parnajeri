b89dbbdfbf0e277f7210c9687f1af61b162d8be1
leocmarques <leo.marques@live.com>
2024-11-20 13:45:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,30 +11,18 @@ st.markdown(
     """)
 
 # Coordenadas centrais do mapa e nível de zoom definidos no código
-#map_center = [-2.8142, -40.4923] 
-#zoom_level = 11  # Nível de zoom inicial do mapa
+map_center = [-2.8142, -40.4923] 
+zoom_level = 11  # Nível de zoom inicial do mapa
 
 # Criar o mapa
-#st.subheader("Mapa Interativo")
-#m = leafmap.Map(center=map_center, zoom=zoom_level, basemap="OpenStreetMap")
+st.subheader("Mapa Interativo")
+m = leafmap.Map(center=map_center, zoom=zoom_level, basemap="OpenStreetMap")
 
 
-
-
-#m.add_geojson("polos_wgs.geojson", layer_name="Polos")
-#m.add_geojson("pontos.geojson", layer_name="Pontos")
+m.add_geojson("polos_wgs.geojson", layer_name="Polos")
+m.add_geojson("pontos.geojson", layer_name="Pontos")
 arvore = "arvore_da_preguica_wgs.tif"
 arvore = leafmap.download_file("https://ambientis.eng.br/jeri/arvore_da_preguica_wgs.tif", "arvore_da_preguica_wgs.tif")
-#m.add_raster("arvore_da_preguica_wgs.tif", layer_name="Árvore da Preguiça")
-
-# Calcular os bounds do raster
-with rasterio.open(arvore) as src:
-    bounds = src.bounds  # Obtém os limites do raster
-    center = [(bounds.top + bounds.bottom) / 2, (bounds.left + bounds.right) / 2]  # Centro
-
-# Criar o mapa ajustado aos bounds
-zoom = 18  # Ajuste o zoom conforme necessário
-m = leafmap.Map(center=center, zoom=zoom)
 m.add_raster("arvore_da_preguica_wgs.tif", layer_name="Árvore da Preguiça")
 
 
@@ -49,13 +37,6 @@ m.add_raster("arvore_da_preguica_wgs.tif", layer_name="Árvore da Preguiça")
 #        <img src="{foto_url}" alt="Foto de {nome}" width="300"/>
 #        """
 #        m.add_marker(location=(coord[1], coord[0]), popup=popup_html)
-
-
-#m.add_tile_layer(
-#    url="https://api.maptiler.com/tiles/7122380a-6c07-4aa6-9266-67b3be263a1b/{z}/{x}/{y}.png?key=siZ1uTKnlAee8SLZokfo",
-#    name="Árvore da Preguiça",
-#    attribution="MapTiler"
-#)
 
 
 # Exibir o mapa
