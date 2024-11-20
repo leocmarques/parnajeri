e50200dce97546bbc1456e801cc52cc46838bb09
leocmarques <leo.marques@live.com>
2024-11-20 10:12:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ zoom_level = 11  # Nível de zoom inicial do mapa
 # Carregar camada vetorial
 try:
     gdf = gpd.read_file(vetorial_url)
-    st.write("Camada vetorial carregada com sucesso. Primeiras 5 linhas:")
-    st.write(gdf.head())
+    #st.write("Camada vetorial carregada com sucesso. Primeiras 5 linhas:")
+    #st.write(gdf.head())
 except Exception as e:
     st.error(f"Erro ao carregar a camada vetorial: {e}")
 
@@ -31,16 +31,16 @@ st.subheader("Mapa Interativo")
 m = leafmap.Map(center=map_center, zoom=zoom_level, basemap="OpenStreetMap")
 
 # Adicionar camada vetorial com popups personalizados
-if 'gdf' in locals():
-    for _, row in gdf.iterrows():
-        nome = row["name"]
-        foto_url = row["photo_url"]
-        coord = row.geometry.coords[0]
-        popup_html = f"""
-        <h4>{nome}</h4>
-        <img src="{foto_url}" alt="Foto de {nome}" width="300"/>
-        """
-        m.add_marker(location=(coord[1], coord[0]), popup=popup_html)
+#if 'gdf' in locals():
+#    for _, row in gdf.iterrows():
+#        nome = row["name"]
+#        foto_url = row["photo_url"]
+#        coord = row.geometry.coords[0]
+#        popup_html = f"""
+#        <h4>{nome}</h4>
+#        <img src="{foto_url}" alt="Foto de {nome}" width="300"/>
+#        """
+#        m.add_marker(location=(coord[1], coord[0]), popup=popup_html)
 
 # Adicionar camada raster ao mapa
 #try:
