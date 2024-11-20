ac86a371651887c07fecf65a70c1c5da6efaeedf
leocmarques <leo.marques@live.com>
2024-11-20 13:57:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ arvore = "arvore_da_preguica_wgs.tif"
 arvore = leafmap.download_file("https://ambientis.eng.br/jeri/arvore_da_preguica_wgs.tif", "arvore_da_preguica_wgs.tif")
 m.add_raster("arvore_da_preguica_wgs.tif", layer_name="Árvore da Preguiça")
 
-mbtiles_to_pmtiles("mbtiles_arvore.mbtiles", "pmtiles_arvore.pmtiles",max_zoom=99)
+leafmap.mbtiles_to_pmtiles("mbtiles_arvore.mbtiles", "pmtiles_arvore.pmtiles",max_zoom=99)
 m.add_pmtiles("pmtiles_arvore.pmtiles", name="Árvore", attribution="Dados PMTiles")
 
 # Adicionar camada vetorial com popups personalizados
